3473a5b49ad80f997476d37f4826b49e5ef2f71b
hendrtre <trentdev2020@gmail.com>
2022-04-11 21:32:49 -0600

    first commit, start of project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Trent's Recipe Book.</h1>
+      <p>Lets get started</p>
+    </div>
+  );
+}
+
+export default App;
