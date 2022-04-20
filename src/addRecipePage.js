a72b29c0f9401ed9abc4c05878d84d908035c833
hendrtre <trentdev2020@gmail.com>
2022-04-19 19:07:16 -0600

    begin stylizing

--- src/addRecipePage.js
+++ src/addRecipePage.js
@@ -5,72 +5,78 @@ function AddRecipePage() {
         <div className='container'>
             <h1>Add New Recipe</h1>
             <div className='form-holder'>
+                {/* <div className='title-form'> */}
                 <form>
-                    <label>
-                        Title: 
-                        <input type="text" name="Title" placeholder='Name of Recipe' />
-                    </label>
-                </form>
+                    <div className='title-form'>
+                        <label>
+                            Title: 
+                            <input type="text" name="Title" placeholder='Name of Recipe...' />
+                        </label>
+                    </div>
 
-                <form>
-                    <label>
-                        Cover Image: 
-                        <input type="file" name='coverImage' />
-                    </label>
-                </form>
+                    <div className='image-form'>
+                        <label>
+                            Cover Image: 
+                            <input type="file" name='coverImage' />
+                        </label>
+                    </div>
 
-                <form>
-                    <label>
-                    Prep Time:  
-                    <input
-                        name="numberOfHoursPrep"
-                        type="number"
-                        placeholder='Hours'
-                        // value={this.state.numberOfGuests}
-                        // onChange={this.handleInputChange} 
-                    />
-                    <input
-                        name="numberOfMinutesPrep"
-                        type="number"
-                        placeholder='Minutes'
-                        // value={this.state.numberOfGuests}
-                        // onChange={this.handleInputChange} 
-                    />
-                    </label>
+                    <div className='time-form'>
+                        <div className='prep-form'>
+                            <label>
+                            Prep Time:  
+                                <input
+                                    name="numberOfHoursPrep"
+                                    type="number"
+                                    placeholder='Hours'
+                                    // value={this.state.numberOfGuests}
+                                    // onChange={this.handleInputChange} 
+                                />
+                                <input
+                                    name="numberOfMinutesPrep"
+                                    type="number"
+                                    placeholder='Minutes'
+                                    // value={this.state.numberOfGuests}
+                                    // onChange={this.handleInputChange} 
+                                />
+                            </label>
+                        </div>
+                        <div className='cook-form'>
+                            <label>
+                            Cook Time:  
+                                <input
+                                    name="numberOfHoursCook"
+                                    type="number"
+                                    placeholder='Hours'
+                                    // value={this.state.numberOfGuests}
+                                    // onChange={this.handleInputChange} 
+                                />
+                                <input
+                                    name="numberOfMinutesCook"
+                                    type="number"
+                                    placeholder='Minutes'
+                                    // value={this.state.numberOfGuests}
+                                    // onChange={this.handleInputChange} 
+                                />
+                            </label>
+                        </div>
+                    </div>
+                    <div className='instruction-form'>
+                        <label>
+                            Steps/Directions:
+                            <textarea placeholder="Write instruction here..." />
+                        </label>
+                    </div>
+                    <div className='measurements-form'>
+                        <label>
+                            Ingredients:
+                            <textarea placeholder='Write ingredient with measurement here...' />
+                        </label>
+                    </div>
                 </form>
-                <form>
-                    <label>
-                    Cook Time:  
-                    <input
-                        name="numberOfHoursCook"
-                        type="number"
-                        placeholder='Hours'
-                        // value={this.state.numberOfGuests}
-                        // onChange={this.handleInputChange} 
-                    />
-                    <input
-                        name="numberOfMinutesCook"
-                        type="number"
-                        placeholder='Minutes'
-                        // value={this.state.numberOfGuests}
-                        // onChange={this.handleInputChange} 
-                    />
-                    </label>
-                </form>
-                <form>
-                    <label>
-                        Steps/Directions:
-                        <textarea></textarea>
-                    </label>
-                </form>
-                <form>
-                    <label>
-                        Ingredients:
-                        <textarea></textarea>
-                    </label>
-                </form>
-
-                <input type="submit" value="Submit" />
+                <div className='finialSubmit'>
+                    <input className='finialSubmitBTN' type="submit" value="+ Add New Recipe" />
+                </div>
             </div>
         </div>
     )
